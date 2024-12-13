05e2cd033f99de44c7d397cf38133b26e92e821e
sergio.pena <sergio.pena.orozco@gmail.com>
2024-12-13 01:01:37 -0600

    add getProducts service

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from 'express'
 import Product from '../models/Product.model'
 
+export const getProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll({
+            order: [
+                ['price', 'DESC']
+            ]
+        })
+        res.json({data: products})
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export const createProduct = async (req: Request, res: Response) => {
-        
-    const product = await Product.create(req.body)    
-    res.json({data: product})
+    try {
+        const product = await Product.create(req.body)    
+        res.json({data: product})
+    } catch (error) {
+        console.error(error)
+    }
 }
