c0f48a0c1171690a9762a45cc469d49a9a872cc9
sergio.pena <sergio.pena.orozco@gmail.com>
2024-12-27 01:00:03 -0600

    add comand for test coverage and tests for patch service

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -182,6 +182,32 @@ describe('PUT /api/products/:id', () => {
     })
 })
 
+describe('PATCH /api/products/:id', () => {
+    it('Should return a 404 response for a non-existent product', async () => {
+        const productId = 2000
+        const response = await request(server).patch(`/api/products/${productId}`)
+
+        expect(response.status).toBe(404)
+        expect(response.body.error).toBe('Product not found')
+
+        expect(response.status).not.toBe(200)
+        expect(response.body).not.toHaveProperty('data')
+        
+    })
+
+    it('Should update the product availability', async () => {
+        const response = await request(server).patch('/api/products/1')
+
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveProperty('data')
+        expect(response.body.data.availability).toBe(false)
+
+        expect(response.status).not.toBe(404)
+        expect(response.status).not.toBe(400)
+        expect(response.body).not.toHaveProperty('error')
+    })
+})
+
 describe('DELETE /api/products/:id', ()  => {
     it('Should check a valid ID in the URL', async () => {
         const response = await request(server).delete('/api/products/not-valid-url')
