aa3ce7f3725350e821470314f017d5201105a98d
sergio.pena <sergio.pena.orozco@gmail.com>
2025-01-16 22:20:25 -0600

    add cors and logger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import colors from 'colors'
+import cors, { CorsOptions } from 'cors'
 import swaggerUi from 'swagger-ui-express'
 import swaggerSpec, { swaggerOptions } from './config/swagger'
 import router from './router'
 import db from './config/db'
+import morgan from 'morgan'
 
 // Connection to DataBase
 export async function connectDB() {
@@ -22,6 +24,23 @@ connectDB()
 
 // Express instance
 const server = express()
+
+// CORS
+const corsOptions: CorsOptions = {
+    origin: function(origin, callback) {
+        if(origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('CORS Error'))
+        }
+    }
+}
+
+server.use(cors(corsOptions))
+
+// Logger
+server.use(morgan('dev'))
+
 // Read data from forms
 server.use(express.json())
 server.use('/api/products', router)
